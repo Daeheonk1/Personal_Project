51e2b439114342a0007e218c6db3e9b5cf394061
Daeheonk1 <118972842+Daeheonk1@users.noreply.github.com>
2023-01-08 11:36:36 +0900

    20230108

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
-import { BrowserRouter, Routes, Route} from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MainPageContainer from "./pages/main/MainPageContainer";
 
 
 
 function App() {
   return (
- <BrowserRouter>
- <Routes>
-  <Route path="/" element={<MainPageContainer/>}/>
- </Routes>
- </BrowserRouter>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MainPageContainer />} />
+        <Route exact path="/page1" element={<div>Page1</div>} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
--- src/pages/main/MainPageContainer.js
+++ src/pages/main/MainPageContainer.js
@@ -1,9 +1,47 @@
-import react from "react";
+import react, { useEffect } from "react";
+import "../../styles/App.css";
+import "../../styles/index.css";
+import { Navigate, useNavigate } from "react-router-dom";
+import Observer from "../../utils/observer"
 
 const MainPageContainer = () => {
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        Observer();
+    }, []);
+
+    const gotopage1 = () => {
+        navigate("/page1")
+    }
+
     return (
-        <div>
-            <h1>Home Page</h1>
+        <div className="container">
+            <div className="h-component hidden">
+                <div className="center">
+
+                </div>
+            </div>
+            <div className=" h-component hidden border">
+                <div className="flex-column">
+                    <div className="hidden">
+                        <img onClick={gotopage1} src="http://blog.jinbo.net/attach/615/200937431.jpg" />
+                        <p> tjfaud</p>
+                    </div>
+                    <div className="hidden">
+                        <p> tjfaud</p>
+                        <img src="http://blog.jinbo.net/attach/615/200937431.jpg" />
+                    </div>
+                </div>
+            </div>
+
+            <div className="h-compntnt hidden">
+                <div className="flex-row">
+                    <img src="http://blog.jinbo.net/attach/615/200937431.jpg" />
+                    <p> Title</p>
+                </div>
+
+            </div>
         </div>
     );
 };
