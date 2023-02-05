2b01c432f1df817b044578fc54ba4b1ae5ec1946
Daeheonk1 <118972842+Daeheonk1@users.noreply.github.com>
2023-02-05 12:13:04 +0700

    2/5/2023

--- src/pages/page1/Page1.js
+++ src/pages/page1/Page1.js
@@ -1,8 +1,11 @@
 import react, { useEffect } from 'react';
 import Observer from "../../utils/observer";
 import Header from "../../common/Header";
+import { P1_Label } from "../../common/lang"
 
 const Page1 = () => {
+    // const lang = useState("en");
+
 
     useEffect(() => {
         Observer();
@@ -15,32 +18,58 @@ const Page1 = () => {
             <div className="h-component hidden">
                 <div className="center"> </div>
             </div>
-            <div className="h-compnent flex-column">
+            <div className="h-compnent flex-column left">
+                <div className="p1_content">
+                    <h1>{P1_Label.EN.ASD_DEF_TITLE} </h1>
+                    <p> {P1_Label.EN.ASD_DEF} </p>
+                </div>
 
-                <h1>
-
-                    What is Austism Spectrum Disorder?
-                </h1>
-                <ul>
-                    <li>
-                        aaaaaaa
-                    </li>
-                    <li>
-                        aaa
-                    </li>
-                </ul>
-                <h1>
-                    What are the main types of this disoder?
-                </h1>
-                <p>
-
-                </p>
-                <h1>
-                    What is Austism Spectrum Disorder?
-                </h1>
-                <p>
-
-                </p>
+                <div className="p1_content">
+                    <h1>{P1_Label.EN.ASD_CLASSIFICATION_TITLE} </h1>
+                    <ul>
+                        <li> {P1_Label.EN.ASD_CLASSIFICATION1}</li>
+                        <li> {P1_Label.EN.ASD_CLASSIFICATION2}</li>
+                        <li> {P1_Label.EN.ASD_CLASSIFICATION3}</li>
+                        <li> {P1_Label.EN.ASD_CLASSIFICATION4}</li>
+                    </ul>
+                </div>
+                <div className="p1_content">
+                    <h1> {P1_Label.EN.ASD_CAUSE_TITLE} </h1>
+                    <p>{P1_Label.EN.ASD_CAUSE_DEF} </p>
+                    <ol>
+                        <li> {P1_Label.EN.ASD_CAUSE_DEF1}</li>
+                        <li> {P1_Label.EN.ASD_CAUSE_DEF2}</li>
+                        <li> {P1_Label.EN.ASD_CAUSE_DEF3}</li>
+                        <li> {P1_Label.EN.ASD_CAUSE_DEF4}</li>
+                    </ol>
+                </div>
+                <div className="p1_content">
+                    <h1> {P1_Label.EN.ASD_SYMPTOMS_TITLE} </h1>
+                    <p>
+                        {P1_Label.EN.ASD_SYMPTOMS_DEF}
+                        <a>{P1_Label.EN.ASD_SYMPTOMS_DEF_LINK}  </a>
+                    </p>
+                    <ol>
+                        <li> {P1_Label.EN.ASD_SYMPTOMS_DEF1}</li>
+                        <li> {P1_Label.EN.ASD_SYMPTOMS_DEF2}</li>
+                        <li> {P1_Label.EN.ASD_SYMPTOMS_DEF3}</li>
+                        <li> {P1_Label.EN.ASD_SYMPTOMS_DEF4}</li>
+                        <li> {P1_Label.EN.ASD_SYMPTOMS_DEF5}</li>
+                        <li> {P1_Label.EN.ASD_SYMPTOMS_DEF6}</li>
+                        <li> {P1_Label.EN.ASD_SYMPTOMS_DEF7}</li>
+                    </ol>
+                </div>
+                <div className="p1_content">
+                    <h1> {P1_Label.EN.ASD_TREATMENTS_TITLE} </h1>
+                    <p>{P1_Label.EN.ASD_TREATMENTS_DEF} </p>
+                    <ol>
+                        <li> {P1_Label.EN.ASD_TREATMENTS_DEF1}</li>
+                        <li> {P1_Label.EN.ASD_TREATMENTS_DEF2}</li>
+                        <li> {P1_Label.EN.ASD_TREATMENTS_DEF3}</li>
+                        <li> {P1_Label.EN.ASD_TREATMENTS_DEF4}</li>
+                        <li> {P1_Label.EN.ASD_TREATMENTS_DEF5}</li>
+                    </ol>
+                </div>
 
             </div>
         </div>);
